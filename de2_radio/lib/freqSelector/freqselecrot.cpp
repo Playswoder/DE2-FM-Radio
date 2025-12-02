0bf336fd7de4ae8d6b53514efb30176529fc8bf9
Playswoder <playswoder.entr@gmail.com>
2025-12-02 22:11:28 +0100

    Delete de2_radio directory

--- de2_radio/lib/freqSelector/freqselecrot.cpp
+++
@@ -1,9 +0,0 @@
-#include "FreqSelector.h"
-
-FreqSelector* FreqSelector::instance = nullptr;
-
-// ISR for PORTD pin change (PCINT2 group)
-ISR(PCINT2_vect)
-{
-    FreqSelector::handleInterrupt();
-}
